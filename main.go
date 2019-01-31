ca747c5f388e3e5522256c29cc2480fb6920b22c
Benjamin28 <Benjamin.Baker@utexas.edu>
2019-01-31 17:33:17 -0500

    basic local web api poc

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+       "encoding/json"
+       "log"
+       "net/http"
+       "github.com/gorilla/mux"
+)
+
+func main() {
+     router := mux.NewRouter()
+
+     people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Status: "Prospect", Address: &Address{City: "City X", State: "State X"}})
+     people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+     people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
+
+     router.HandleFunc("/people", GetPeople).Methods("GET")
+     router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+     router.HandleFunc("/people/{id}/status", GetStatus).Methods("GET")
+     router.HandleFunc("/people/{id}/status/{newstatus}", SetStatus).Methods("PUT")
+
+     router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+     router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+
+     log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+type Person struct {
+       ID           string    `json:"id,omitempty"`
+       Firstname    string    `json:"firstname,omitempty"`
+       Lastname     string    `json:"lastname,omitempty"`
+       Status       string    `json:"status,omitempty`
+       Address      *Address  `json:"address,omitempty"`
+}
+
+type Address struct {
+     City           string    `json:"city,omitempty"`
+     State          string    `json:"state,omitempty"`
+}
+
+var people []Person
+
+func GetPeople(w http.ResponseWriter, r *http.Request) {
+     json.NewEncoder(w).Encode(people)
+}
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+     params := mux.Vars(r)
+     for _, item := range people {
+          if item.ID == params["id"] {
+               json.NewEncoder(w).Encode(item)
+               return
+          }
+     }
+     json.NewEncoder(w).Encode(&Person{})
+}
+
+func GetStatus(w http.ResponseWriter, r *http.Request) {
+     params := mux.Vars(r)
+     for _, item := range people {
+          if item.ID == params["id"] {
+               json.NewEncoder(w).Encode(item.Status)
+               return
+          }
+     }
+     json.NewEncoder(w).Encode("")
+}
+
+func SetStatus(w http.ResponseWriter, r *http.Request) {
+     params := mux.Vars(r)
+     for _, item := range people {
+          if item.ID == params["id"] {
+               item.Status = params["newstatus"]
+               json.NewEncoder(w).Encode(people)
+               return
+          }
+     }
+     json.NewEncoder(w).Encode("User with given id not found")
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+     params := mux.Vars(r)
+     var person Person
+     _ = json.NewDecoder(r.Body).Decode(&person)
+     person.ID = params["id"]
+     people = append(people, person)
+     json.NewEncoder(w).Encode(people)
+}
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+}
